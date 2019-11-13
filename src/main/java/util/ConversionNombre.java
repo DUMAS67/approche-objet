fbdd13e6f32d82e98808dce11472ff1e4e762c10
DUMAS Thierry <afthierry_dumas@yahoo.com>
2019-11-13 17:47:40 +0100

    TP4

---
+++ src/main/java/util/ConversionNombre.java
@@ -0,0 +1,20 @@
+package util;
+
+import java.lang.*;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 20;
+
+		String blabla = "8";
+		int huit = Integer.parseInt(blabla);
+		int max = Integer.max(a, b);
+
+		System.out.println(huit);
+		System.out.println(max);
+	}
+
+}
