f4eaef2b52d941a8d980767f9c8760b236015f55
DUMAS Thierry <afthierry_dumas@yahoo.com>
2019-11-22 22:05:54 +0100

    TP5 Fichier - Initial

---
+++ src/main/java/fr/diginamic/fichier/LectureFichier.java
@@ -0,0 +1,24 @@
+package fr.diginamic.fichier;
+
+import java.io.File;
+import java.util.List;
+import org.apache.commons.io.FileUtils;
+
+public class LectureFichier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			File fichierRecensement = new File("C:/tempsts/recensement_population_2016.CSV");
+			List<String> lignes = FileUtils.readLines(fichierRecensement, "UTF-8");
+			
+			for ( String ligne :lignes){
+				System.out.println("Nombre de lignes : " +ligne);
+			}
+			System.out.println(lignes.size());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
